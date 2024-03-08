23c84c6f529ae4e0f88b3fc8c66fc2568ede8536
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-03-08 11:49:44 -0300

    eqwq

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var nome_Usuario = '';
 var texto = document.querySelector('span');
 
-while(nome_Usuario == ''){
+while(nome_Usuario == '' || nome_Usuario == null){
     nome_Usuario = prompt('Digite seu apelido aqui!');
 }
 
