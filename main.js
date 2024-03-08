4772764fa307249095fae66e60c63e79627ec9d3
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-03-08 11:54:22 -0300

    kuk

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var nome_Usuario = '';
 var texto = document.querySelector('span');
 
-while(nome_Usuario == '' || nome_Usuario == null){
-    nome_Usuario = prompt('Digite seu apelido aqui!');
+while(nome_Usuario == '' ){ // || nome_Usuario == null
+nome_Usuario = prompt('Digite seu apelido aqui!');
 }
 
 texto.textContent = nome_Usuario;
