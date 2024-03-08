8a2491ca51a409e1a83add3dabf24dcf989fd169
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-03-08 12:16:09 -0300

    d

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var nome_Usuario = '';
 var texto = document.querySelector('span');
 
 while(nome_Usuario == '' ){ // || nome_Usuario == null
-nome_Usuario = prompt('Digite seu apelido aqui!');
+nome_Usuario = prompt('Digite seu apelido aqui! ":)"');
 }
 
 if(nome_Usuario == null){
