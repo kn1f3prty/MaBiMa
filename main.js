4f759a548a2fd7571f1320a29d80a4ecfbb02262
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-03-08 12:02:42 -0300

    ewe

--- main.js
+++ main.js
@@ -5,4 +5,10 @@ while(nome_Usuario == '' ){ // || nome_Usuario == null
 nome_Usuario = prompt('Digite seu apelido aqui!');
 }
 
+if(nome_Usuario == null){
+texto.textContent = 'usuário';
+}else{
 texto.textContent = nome_Usuario;
+}
+
+
