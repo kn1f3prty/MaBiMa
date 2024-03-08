fca4688137a4c37daa4491739eeeffa4c1465b23
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-03-08 11:43:00 -0300

    -0-0

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var nome_Usuario = prompt('Digite seu apelido aqui!');
+var nome_Usuario = '';
 var texto = document.querySelector('span');
 
 texto.textContent = nome_Usuario;
+
+while(nome_Usuario == ''){
+    nome_Usuario = prompt('Digite seu apelido aqui!');
+}
