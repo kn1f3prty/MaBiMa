b88707b2cf0e2eab23a1e6febd27d336427697cb
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-03-08 11:45:08 -0300

    32323

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var nome_Usuario = '';
 var texto = document.querySelector('span');
 
-texto.textContent = nome_Usuario;
-
 while(nome_Usuario == ''){
     nome_Usuario = prompt('Digite seu apelido aqui!');
 }
+
+texto.textContent = nome_Usuario;
