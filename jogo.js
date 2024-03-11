e4ccae7db7ab19985a3c47547abe55ddbeaf091c
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-03-11 11:55:01 -0300

    fg

--- jogo.js
+++ jogo.js
@@ -2,11 +2,13 @@ let numeroSecreto = 13;
 
 let chute = '';
 
-while(chute == ''){
+while(chute == '' || chute== null){
     chute = prompt('Escolha um numero de 0 a 20');
 }
 
 /* prompt é o comando usado para enviar uma mensagem */
+
+
 
 if(numeroSecreto == chute){
     alert ('Parabéns! Você acertou:');
