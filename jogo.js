15e840732b5cfd4ebc8b07ba3adc2092eff12f9c
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-04-01 11:23:53 -0300

    ere

--- jogo.js
+++ jogo.js
@@ -4,10 +4,8 @@ console.log(numeroSecreto);
 let chute = '';
 
 while(chute == '' || chute== null){
-    chute = prompt('Escolha um numero de 0 a 20');
+    chute = prompt('Escolha um numero de 0 a 20');  /* prompt é o comando usado para enviar uma mensagem */
 }
-
-/* prompt é o comando usado para enviar uma mensagem */
 
 if(numeroSecreto == chute){
     alert ('Parabéns! Você acertou:)');
