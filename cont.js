f4b882d1c267477c744676ea66c73fefe3156f20
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-04-11 08:54:41 -0300

    sas

--- cont.js
+++ cont.js
@@ -5,7 +5,7 @@ let i = 0;
 
 for(let i = 0; i<botoes.length; i++){
 console.log(i);
-botoes[i].onclick = functio(){
+botoes[i].onclick = function(){
     botoes[i].classList.add('ativo');
     //botoes[i].classList.remove('ativo');
 }
