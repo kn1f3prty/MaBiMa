b1d0209b4dfcc559fc7705ad4f5a629909894a10
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-04-11 09:00:42 -0300

    ewew

--- cont.js
+++ cont.js
@@ -6,7 +6,9 @@ let i = 0;
 for(let i = 0; i<botoes.length; i++){
 console.log(i);
 botoes[i].onclick = function(){
+    for (let j = 0; j<botoes.length; j++){
+    botoes[j].classList.remove('ativo');
+    }
     botoes[i].classList.add('ativo');
-    //botoes[i].classList.remove('ativo');
 }
 }
