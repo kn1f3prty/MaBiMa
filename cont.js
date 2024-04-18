033bd428f75ac0460e6b7bbad135f90b4b8a9752
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-04-18 10:26:48 -0300

    jg

--- cont.js
+++ cont.js
@@ -17,7 +17,7 @@ botoes[i].onclick = function(){
 }
 const contadores = document.querySelectorAll('.contador');
 let tempoAtual = new Date ();
-let tempoObjetivo1 = new Date ('2024-04-19T00:00:00')
+let tempoObjetivo1 = new Date ('2024-04-19T00:00:00');
 let tempoFinal = tempoFinal - tempoAtual;
 let segundos =  tempoFinal/1000;
 let minutos = segundos/60;
