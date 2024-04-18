a3464dff41a52540d4c1abe5683cbd7f35676b49
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-04-18 09:36:42 -0300

    eses

--- cont.js
+++ cont.js
@@ -16,5 +16,7 @@ botoes[i].onclick = function(){
     }
 }
 const contadores = document.querySelectorAll('.contador');
+let tempoAtual = new Date ();
+let tempoObjetivo1 = new Date ('2024-04-19T00:00:00')
 
-contadores[0].textContent = "oi, tudo bem?";
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
